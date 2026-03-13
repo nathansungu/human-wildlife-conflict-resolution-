4814598ce32b00809663b46632fa29e04faaa477
nathansungu <nathanamudavi.com>
2026-03-13 10:06:02 +0300

    add login and refresh token endpoints

--- backend/src/services/animals.Service.ts
+++ backend/src/services/animals.Service.ts
@@ -1,12 +1,2 @@
-import prismaInstance from "../prismaInstance";
 
-export const addAnimalService = async (name: string, conservationStatus: string, alertPriority: string) => {
-    const newAnimal = await prismaInstance.animals.create({
-        data: { name, conservationStatus, alertPriority }
-    })
-    if (!newAnimal) {
-        return Promise.reject(new Error("failed to add animal"))
-    }
-    return newAnimal;   
-}
 
